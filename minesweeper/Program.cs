c68ed4699206a27b31576564e54f03f7a583d8a3
Shaun Levy <SLY@ndonline.ac.uk>
2025-01-14 16:07:03 +0000

    ...

--- minesweeper/Program.cs
+++ minesweeper/Program.cs
@@ -2,18 +2,67 @@
 {
     internal class Program
     {
+        static bool gameOver = false;
+        static Grid myGrid;
+        static bool firstTurn = true;
         static void Main(string[] args)
         {
-            Grid myGrid = new Grid(25, 25);
+            myGrid = new Grid(25, 25);
             myGrid.placeMines();
-            draw(myGrid);
+            
+            loop();
+
         }
         static void draw(Grid grid)
         {
             Console.Clear();
             Console.SetCursorPosition(0, 0);
             Console.WriteLine(grid.drawGrid());
+        }
+        static void loop()
+        {
+            //first turn
 
+            while (!gameOver)
+            {
+                draw(myGrid);
+                Console.WriteLine("your turn:");
+                Console.WriteLine("Options: R, Reveal; F, Flag");
+                string choice = Console.ReadLine();
+                Console.WriteLine("enter x coord:");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter y coord:");
+                int y = int.Parse(Console.ReadLine());
+                if (choice == "F")
+                {
+                    myGrid.getSquare(x, y).addFlag();
+                    myGrid.getSquare(x, y).reveal();
+                }
+                else if (choice == "R")
+                {
+                    if (firstTurn)
+                    {
+                        for (int i = x - 5; i < x + 5; i++)
+                        {
+                            for (int j = y - 5; j < y + 5; j++)
+                            {
+                                if (i >= 0 && i < 25 && j >= 0 && j < 25)
+                                {
+                                    myGrid.getSquare(i, j).clearMine();
+                                }
+                            }
+                        }
+
+                        myGrid.getSquare(x, y).reveal();
+                        
+                        firstTurn = false;
+                    }
+                    else
+                    {
+                        myGrid.getSquare(x, y).reveal();
+                    }
+                }
+            }
         }
     }
     class Square
@@ -37,6 +86,10 @@
         {
             mine = true;
         }
+        public void clearMine()
+        {
+            mine = false;
+        }
         public bool getFlag()
         {
             return flag;
@@ -56,6 +109,14 @@
         public void setNeighbours(int _neighbours)
         {
             neighbours = _neighbours;
+        }
+        public void reveal()
+        {
+            revealed = true;
+        }
+        public bool getRevealed()
+        {
+            return revealed;
         }
     }
     class Grid
@@ -84,9 +145,13 @@
             //neighbours = new int[width, height];
             rnd = new Random(1234);
         }
+        public Square getSquare(int x, int y)
+        {
+            return squares[x, y];
+        }
         public void placeMines()
         {
-            int numberOfMines = (width * height) / 20;
+            int numberOfMines = (width * height) / 10;
             for (int i = 0; i < numberOfMines; i++)
             {
                 int mineX = rnd.Next(0, width);
@@ -105,6 +170,16 @@
             }
             calculateNeighbours();
         }
+        public void revealAll()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    squares[i, j].reveal();
+                }
+            }
+        }
         public string drawGrid()
         {
             string output = "";
@@ -115,7 +190,21 @@
                     //output += !revealed[x, y] ? "." : grid[x, y];
                     //output += grid[x,y] == 1 ? "@" : neighbours[x, y].ToString();
                     //output += squares[x, y].getMine() ? "@" : squares[x, y].getNeighbours().ToString();
-                    output += squares[x, y].getNeighbours().ToString();
+                    if (squares[x, y].getRevealed())
+                    {
+                        if ((squares[x, y].getMine() || squares[x, y].getFlag()))
+                        {
+                            output += squares[x, y].getMine() ? "@" : "F";
+                        }
+                        else
+                        {
+                            output += squares[x, y].getNeighbours().ToString();
+                        }
+                    }
+                    else
+                    {
+                        output += ".";
+                    }
                 }
                 output += "\n";
             }
